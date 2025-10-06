422e75530ce3f8597a929027bcbf9b4b1942e6ad
zohaibsaeed117 <zohaib.saeed1259@gmail.com>
2025-10-06 09:46:25 +0500

    added tailwindcss

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
 import { Text, View } from "react-native";
+import "../global.css";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      className="flex items-center justify-center h-screen"
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-2xl text-red-500">Welcome to Aesthetica</Text>
     </View>
   );
 }
